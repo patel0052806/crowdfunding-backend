574e0c4ddda7df0298ce6bce523ebd3b4203fe02
taufik <pateltaufik266@gmail.com>
2026-01-18 21:31:45 +0530

    email verification

--- controllers/auth-controller.js
+++ controllers/auth-controller.js
@@ -63,7 +63,7 @@ const sendOtp = async (req, res) => {
     }
     catch (error) {
         console.error('Error sending OTP:', error);
-        res.status(500).json({ message: 'Error sending OTP' });
+        res.status(500).json({ message: 'Error sending OTP', error: error.message });
     }
 };
 
